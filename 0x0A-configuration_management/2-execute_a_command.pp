7515d0ab212b21f566dc1ac042d3299e43e4d6f3
Wangui-quinnie <w.wangui@ymail.com>
2023-11-27 10:13:51 -0800

    Execute a command

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+# Create a manifest that kills a process named killmenow
+exec { 'kill_killmenow_process':
+  command => 'pkill -f killmenow',
+  path    => ['/bin', '/usr/bin'],
+  onlyif  => 'pgrep -f killmenow',
+}
