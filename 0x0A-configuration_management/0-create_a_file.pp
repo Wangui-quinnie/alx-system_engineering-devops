c8f986aa1b235073c24c7da0121eb12fea162b12
Wangui-quinnie <w.wangui@ymail.com>
2023-11-27 09:13:58 -0800

    Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# To create a Puppet manifest file
+file { '/tmp/school':
+  ensure  => present,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
