1e23372ec634919e3f037ab23655199b534410ef
Wangui-quinnie <w.wangui@ymail.com>
2024-01-22 08:00:52 -0800

    0x19. Postmortem

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -16,15 +16,17 @@ def top_ten(subreddit):
     Returns:
         None
     """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
-    params = {'limit': 10}
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/Altruistic_Motor_947)"
+    }
+    params = {
+        "limit": 10
+    }
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
-    if response.status_code != 200:
-        print(None)
-    else:
-        data = response.json()
-        posts = data['data']['children']
-        for post in posts:
-            print(post['data']['title'])
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
---
+++ 0x19-postmortem/README.md
@@ -0,0 +1,1 @@
+https://docs.google.com/document/d/1XtkH3n6IcktbZ0jYU7tPrgYOORy7ZhKuI4H1t-YWDAo/edit?usp=sharing
